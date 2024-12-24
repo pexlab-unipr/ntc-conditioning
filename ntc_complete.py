a64bc130fd4fb522584b72043dc1bd35c3893e11
ale <alex89.ficus@gmail.com>
2024-12-25 00:20:33 +0100

    Model refactoring for thermal model support
    * Added support for thermal model in the base Bipole class (implicit equation, not explicit)
    * Added temperature dependence of relevant parameters in Diode and NTC models
    * Created a function to update TSEP prior to calculation
    * Added the possibility to compute the models temporarily overriding the default model
    * Added missing info in the LICENSE file

--- ntc_complete.py
+++ ntc_complete.py
@@ -9,42 +9,61 @@ import matplotlib.pyplot as plt
 class Bipole:
     def __init__(self):
         self.par = {}
-        self.temperature = 0
-    def r_model(self, ix):
+        self.T_base = 0.0
+    def r_model(self, ix, Tx=None, model="default"):
         pass
-    def g_model(self, vx):
+    def g_model(self, vx, Tx=None, model="default"):
         pass
-    def thermal_model(self, vx, ix):
-        pass
+    def thermal_model(self, v_or_i, input, Tx):
+        if input == "current":
+            ix = v_or_i
+            p_loss = self.r_model(ix, Tx) * ix
+        elif input == "voltage":
+            vx = v_or_i
+            p_loss = self.g_model(vx, Tx) * vx
+        else:
+            raise ValueError("Invalid model requested")
+        # We are not computing the temperature, but we are writing an equation that must be solved
+        return self.T_base + self.par['Rth'] * p_loss - Tx
 
 class Diode(Bipole):
-    def __init__(self, Is=1e-15, Eta=1, Rth=100, Tdef=300, Model_type="pure_exp"):
+    def __init__(self, Is=1e-15, Eta=1, Rth=100, T_base=300, Model_type="pure_exp"):
         super().__init__()
         self.par['Is'] = Is
         self.par['Eta'] = Eta
         self.par['Rth'] = Rth
         self.par['Model_type'] = Model_type
-        self.set_temperature(Tdef)
-    def set_temperature(self, temperature):
-        self.temperature = temperature
-        # Compute thermal voltage according to current temperature
-        self.Vt = spc.Boltzmann * self.temperature/spc.e
-    def r_model(self, id):
+        self.par['Eg'] = 1.12 * spc.eV # TODO: make it a parameter
+        self.T_base = T_base
+    def __update_TSEP(self, Tx):
+        # Compute temperature sensitive parameters according to the specified temperature
+        # If temperature not specified, use the one stored in the object
+        if Tx is None:
+            Tx = self.T_base
+        self.Vt = spc.Boltzmann * Tx/spc.e
+        self.Is = self.par['Is'] * np.exp(-self.par['Eg']/spc.Boltzmann/Tx)
+    def r_model(self, ix, Tx=None, model="default"):
+        self.__update_TSEP(Tx)
         # Implement various models
-        match self.par['Model_type']:
+        # default can be overriden by the user to reuse expressions
+        # Otherwise set by Model_type
+        if model == "default":
+            model = self.par['Model_type']
+        match model:
             case "pure_exp":
-                return self.par['Eta'] * self.Vt * np.log(id/self.par['Is'])
+                return self.par['Eta'] * self.Vt * np.log(ix/self.par['Is'])
             case "negative_saturation":
-                return self.par['Eta'] * self.Vt * np.log(1.0 + id/self.par['Is'])
+                return self.par['Eta'] * self.Vt * np.log(1.0 + ix/self.par['Is'])
             case _:
                 raise ValueError("Unimplemented diode model requested")
-    def g_model(self, vd):
-        # Implement various models
-        match self.par['Model_type']:
+    def g_model(self, vx, Tx=None, model="default"):
+        if model == "default":
+            model = self.par['Model_type']
+        match model:
             case "pure_exp":
-                return self.par['Is'] * np.exp(vd/self.par['Eta']/self.Vt)
+                return self.par['Is'] * np.exp(vx/self.par['Eta']/self.Vt)
             case "negative_saturation":
-                return self.par['Is'] * np.exp(vd/self.par['Eta']/self.Vt - 1.0)
+                return self.par['Is'] * np.exp(vx/self.par['Eta']/self.Vt - 1.0)
             case _:
                 raise ValueError("Unimplemented diode model requested")
 
@@ -55,25 +74,25 @@ class NTC(Bipole):
         self.par['Beta'] = Beta
         self.par['T0'] = T0
         self.par['Model_type'] = Model_type
-        self.temperature = T0
-    def r_model(self, ix, temperature=None):
-        return self.r_value(temperature) * ix
-    def g_model(self, vx, temperature=None):
-        return 1/self.r_value(temperature) * vx
-    def r_value(self, temperature=None):
-        if np.all(temperature == None):
-            temp_now = self.temperature
-        else:
-            temp_now = temperature
-        match self.par['Model_type']:
+        self.T_base = T0
+    def r_model(self, ix, Tx=None, model="default"):
+        return self.r_value(Tx, model) * ix
+    def g_model(self, vx, Tx=None, model="default"):
+        return 1/self.r_value(Tx, model) * vx
+    def r_value(self, Tx=None, model="default"):
+        if Tx is None:
+            Tx = self.T_base
+        if model == "default":
+            model = self.par['Model_type']
+        match model:
             case "beta_value":
-                return self.par['R0'] * np.exp(self.par['Beta'] * (1/temp_now - 1/self.par['T0']))
+                return self.par['R0'] * np.exp(self.par['Beta'] * (1/Tx - 1/self.par['T0']))
             case "steinhart_hart":
-                x = 1/self.par['C'] * (self.par['A'] - 1/temp_now)
+                x = 1/self.par['C'] * (self.par['A'] - 1/Tx)
                 y = np.sqrt((self.par['B']/(3*self.par['C']))**3 + x**2/4)
                 return np.exp((y - x/2)**(1/3) - (y + x/2)**(1/3))
             case "polynomial":
-                return self.par['R0'] * np.exp(np.polyval(self.par['DCBA'], 1/temp_now))
+                return self.par['R0'] * np.exp(np.polyval(self.par['DCBA'], 1/Tx))
             case _:
                 raise ValueError("Unimplemented NTC model requested")
 
