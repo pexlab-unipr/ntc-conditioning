1da2407bf355f09118a3c0d4a25eac301a63788f
ale <alex89.ficus@gmail.com>
2024-12-31 18:16:58 +0100

    Working on models
    * Models put out of simulation, are specified as parameters
    * Minor changes to parameters
    * Still need to automatize the model generation from measured data

--- ntc_complete.py
+++ ntc_complete.py
@@ -96,7 +96,7 @@ class Diode(Bipole):
                 raise ValueError("Unimplemented diode model requested")
 
 class NTC(Bipole):
-    def __init__(self, R0=10e3, Beta=3780, T0=spc.convert_temperature(25, 'Celsius', 'Kelvin'), Rth= 300, Model_type='beta_value'):
+    def __init__(self, R0=10e3, Beta=3988, T0=spc.convert_temperature(25, 'Celsius', 'Kelvin'), Rth=333, Model_type='beta_value'):
         super().__init__()
         self.par['R0'] = R0
         self.par['Beta'] = Beta
@@ -124,43 +124,44 @@ class NTC(Bipole):
 
 # Simulation parameters
 class NTC_simulation:
-    def __init__(self):
+    def __init__(self, V_bias, N_j, diode, ntc, name="NTC_simulation"):
         self.T_min_deg = -50
         self.T_max_deg = 150
         self.T_base_deg = 25
         self.N_sim = 101
-        self.V_bias = 3.3
-        self.N_j = 4
-        self.diode0 = Diode(Is = 25e-9, Eta = 1.6, Model_type="negative_saturation")
-        self.ntc0 = NTC()
+        self.V_bias = V_bias
+        self.N_j = N_j
+        self.diode = diode
+        self.ntc = ntc
+        self.name = name
         self.T_eval = np.linspace(self.T_min_deg, self.T_max_deg, self.N_sim, endpoint=True)
         self.T_eval = spc.convert_temperature(self.T_eval, 'Celsius', 'Kelvin')
         self.T_base = spc.convert_temperature(self.T_base_deg, 'Celsius', 'Kelvin')
     def get_temperatures(self):
         return (self.T_eval, spc.convert_temperature(self.T_eval, 'Kelvin', 'Celsius'))
     def analysis_ideal_diode(self):
-        vx = self.N_j * (self.diode0.r_model(self.V_bias/self.ntc0.par['R0'], self.T_base, model="pure_exp") - 
-                        self.diode0.par['Eta'] * self.diode0.Vt(self.T_base) * np.log(self.ntc0.r_value(self.T_eval, model="beta_value")/self.ntc0.par['R0']))
-        ix = self.diode0.g_model(vx/self.N_j, self.T_base, model="pure_exp")
+        vx = self.N_j * (self.diode.r_model(self.V_bias/self.ntc.par['R0'], self.T_base, model="pure_exp") - 
+                        self.diode.par['Eta'] * self.diode.Vt(self.T_base) * np.log(self.ntc.r_value(self.T_eval, model="beta_value")/self.ntc.par['R0']))
+        ix = self.diode.g_model(vx/self.N_j, self.T_base, model="pure_exp")
         return (vx, ix)
     def analysis_divider(self):
-        vx = self.V_bias - self.N_j * self.diode0.par['Eta'] * self.diode0.Vt(self.T_base) * sps.lambertw(
-            self.ntc0.r_value(self.T_eval, model="beta_value")/(self.N_j * self.diode0.par['Eta'] * self.diode0.Vt(self.T_base)) * 
-            self.diode0.g_model(self.V_bias/self.N_j, self.T_base, model="pure_exp")
+        vx = self.V_bias - self.N_j * self.diode.par['Eta'] * self.diode.Vt(self.T_base) * sps.lambertw(
+            self.ntc.r_value(self.T_eval, model="beta_value")/(self.N_j * self.diode.par['Eta'] * self.diode.Vt(self.T_base)) * 
+            self.diode.g_model(self.V_bias/self.N_j, self.T_base, model="pure_exp")
         )
-        ix = self.diode0.g_model(vx/self.N_j, self.T_base, model="pure_exp")
+        ix = self.diode.g_model(vx/self.N_j, self.T_base, model="pure_exp")
         return (vx, ix)
     def sim_ideal_diode(self):
         # Ideal diode (diode voltage not affecting NTC voltage)
-        ix = self.ntc0.g_model(self.V_bias, self.T_eval)
-        vx = self.N_j * self.diode0.r_model(ix, self.T_base)
+        ix = self.ntc.g_model(self.V_bias, self.T_eval)
+        vx = self.N_j * self.diode.r_model(ix, self.T_base)
         return (vx, ix) # output voltage and diodes current
     def sim_divider(self, NTC_selfheat=False, Diode_selfheat=False):
         # Asymmetrical voltage divider
         # cir = lambda ix: Vbias - ntc0.r_model(ix, Tx) - Nj*diode0.r_model(ix) # KVL
-        eq_circuit  = lambda vx, T_NTC, T_diode: self.ntc0.g_model(self.V_bias - vx, T_NTC) - self.diode0.g_model(vx/self.N_j, T_diode) # KCL
-        eq_ntc_sh   = lambda vx, T_NTC, T_NTC0: self.ntc0.thermal_model(self.V_bias - vx, "voltage", T_NTC0, T_NTC, NTC_selfheat)
-        eq_diode_sh = lambda vx, T_diode, T_diode0: self.diode0.thermal_model(vx/self.N_j, "voltage", T_diode0, T_diode, Diode_selfheat)
+        eq_circuit  = lambda vx, T_NTC, T_diode: self.ntc.g_model(self.V_bias - vx, T_NTC) - self.diode.g_model(vx/self.N_j, T_diode) # KCL
+        eq_ntc_sh   = lambda vx, T_NTC, T_NTC0: self.ntc.thermal_model(self.V_bias - vx, "voltage", T_NTC0, T_NTC, NTC_selfheat)
+        eq_diode_sh = lambda vx, T_diode, T_diode0: self.diode.thermal_model(vx/self.N_j, "voltage", T_diode0, T_diode, Diode_selfheat)
         eqs_system  = lambda vx, T_NTC, T_diode, T_NTC0, T_diode0: np.array([
             eq_circuit(vx, T_NTC, T_diode), 
             eq_ntc_sh(vx, T_NTC, T_NTC0), 
@@ -170,17 +171,21 @@ class NTC_simulation:
             lambda T_meas: spo.fsolve(
                 lambda x: eqs_system(
                     x[0], x[1], x[2], T_meas, self.T_base), 
-                [0.5*self.N_j, T_meas, self.T_base], 
-                diag=[0.5*self.N_j, T_meas, self.T_base]), 
+                [0.75*self.N_j, T_meas, self.T_base], 
+                diag=[0.75*self.N_j, T_meas, self.T_base]), 
             signature='()->(3)')(self.T_eval)
         vx = x[:,0]
         T_NTC = x[:,1]
         T_diode = x[:,2]
-        ix = self.diode0.g_model(vx/self.N_j, T_diode)
+        ix = self.diode.g_model(vx/self.N_j, T_diode)
         return (vx, ix, T_NTC, T_diode)
 
 # Simulation
-mysim = NTC_simulation()
+diode_1N4148 = Diode(Is =  2.7e-9, Eta = 1.8, Rth = 350, Model_type="negative_saturation")
+diode_1N4007 = Diode(Is = 500e-12, Eta = 1.5, Rth =  93, Model_type="negative_saturation")
+diode_BC817  = Diode(Is =  20e-15, Eta = 1.0, Rth = 160, Model_type="negative_saturation")
+ntc_B57703M_10k = NTC()
+mysim = NTC_simulation(3.3, 1, diode_BC817, ntc_B57703M_10k, name="BC817")
 Tx, Tx_deg = mysim.get_temperatures()
 v_out = np.empty((mysim.N_sim, 4))
 i_ntc = np.empty_like(v_out)
